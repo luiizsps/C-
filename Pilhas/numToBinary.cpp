770032c47b95a76f0059784234cc3f46209aa54c
Luiz Passos <lsps1019@gmail.com>
2023-03-10 11:35:58 -0300

    Update numToBinary.cpp

--- Pilhas/numToBinary.cpp
+++ Pilhas/numToBinary.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void addPilha(int v[], int &topo, int valor);
+void addPilha(int v[], int &topo, int valor, int n);
 void imprimePilha(int v[], int topo);
-void numToBinary(int v[], int &topo, int valor);
+void numToBinary(int v[], int &topo, int valor, int n);
 
 int main(void) {
   int v[20], valor, topo = -1, n;
@@ -12,18 +12,23 @@ int main(void) {
   cout << "Informe um inteiro: ";
   cin >> valor;
   if(topo < (n-1)) {
-      numToBinary(v, topo, valor);
+      numToBinary(v, topo, valor, n);
   }
   imprimePilha(v, topo);
 }
-void addPilha(int v[], int &topo, int valor) {
-  topo++;  
-  v[topo] = valor;
+void addPilha(int v[], int &topo, int valor, int n) {
+  if(topo < (n-1)) {
+    topo++;  
+    v[topo] = valor;
+  } else {
+   cout << "Lista cheia." << endl;
+    return;
+  }
 }
 
-void numToBinary(int v[], int &topo, int valor) {
+void numToBinary(int v[], int &topo, int valor, int n) {
   do {
-    addPilha(v, topo, valor % 2);
+    addPilha(v, topo, valor % 2, n);
     valor /= 2;
   } while(valor > 0);
 }
